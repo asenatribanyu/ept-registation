7ef05e62bdc0ed83a135be4e0cd5ea690b36af1e
asenatribanyu <asenatribanyu@gmail.com>
2023-12-15 19:24:33 +0700

    menambahkan qr code ke sertifikat yang akan di download

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -25,45 +25,71 @@ class Search extends CI_Controller
 			'dataFound' => $dataFound
 		));
 	}
-	public function certificate($id) {
-		$data = $this->db->get_where('tbl_score', array('id_score' => $id))->row();
-		
-		$templatePath = 'assets/sertifikat/TemplateCertificate.png';
-		
-		$certificateImage = imagecreatefrompng($templatePath);
-		
-		$black = imagecolorallocate($certificateImage, 0, 0, 0);
-		
-		$fontPath = FCPATH . 'assets/font/PlaypenSans-VariableFont_wght.ttf';
-		
-		$nama = $data->nama;
-		
-		$imageWidth = imagesx($certificateImage);
-		
-		// Set a maximum width for the text box
-		$maxTextWidth = 0.8 * $imageWidth; // Adjust the scale factor as needed
-		
-		$fontSize = 90; // Set your initial font size
-		$fontScale = 1.0;
-	
-		do {
-			$fontSize *= $fontScale;
-			$textBoundingBox = imagettfbbox($fontSize, 0, $fontPath, $nama);
-			$textWidth = $textBoundingBox[2] - $textBoundingBox[0];
-			$fontScale = $maxTextWidth / $textWidth;
-		} while ($textWidth > $maxTextWidth);
-		
-		// Calculate text position
-		$x = ($imageWidth - $textWidth) / 2;
-		$y = 707;
-		
-		imagettftext($certificateImage, $fontSize, 0, $x, $y, $black, $fontPath, $nama);
-		
-		header('Content-Type: image/png');
-		header('Content-Disposition: attachment; filename="certificate.png"');
-		imagepng($certificateImage);
-		
-		imagedestroy($certificateImage);
-	}
+	public function certificate($id)
+{
+    $this->load->library('ciqrcode');
+
+    $data = $this->db->get_where('tbl_score', array('id_score' => $id))->row();
+
+    // Adjust the size parameter for the QR code
+    $params['data'] = 'https://ept-lembagabahasa.widyatama.ac.id/registration/search';
+    $params['level'] = 'H';
+    $params['size'] = 4; // Adjust the size to 4
+    $params['savename'] = 'assets/sertifikat/' . 'qr.png';
+    $this->ciqrcode->generate($params);
+
+    $qrPath = 'assets/sertifikat/qr.png';
+
+    $templatePath = 'assets/sertifikat/TemplateCertificate.png';
+
+    $certificateImage = imagecreatefrompng($templatePath);
+
+    $black = imagecolorallocate($certificateImage, 0, 0, 0);
+
+    $fontPath = FCPATH . 'assets/font/PlaypenSans-VariableFont_wght.ttf';
+
+    $nama = $data->nama;
+
+    $imageWidth = imagesx($certificateImage);
+
+    // Set a maximum width for the text box
+    $maxTextWidth = 0.8 * $imageWidth; // Adjust the scale factor as needed
+
+    $fontSize = 90; // Set your initial font size
+    $fontScale = 1.0;
+
+    do {
+        $fontSize *= $fontScale;
+        $textBoundingBox = imagettfbbox($fontSize, 0, $fontPath, $nama);
+        $textWidth = $textBoundingBox[2] - $textBoundingBox[0];
+        $fontScale = $maxTextWidth / $textWidth;
+    } while ($textWidth > $maxTextWidth);
+
+    // Calculate text position
+    $x = ($imageWidth - $textWidth) / 2;
+    $y = 707;
+
+    imagettftext($certificateImage, $fontSize, 0, $x, $y, $black, $fontPath, $nama);
+
+    // Load the QR code image
+    $qrImage = imagecreatefrompng($qrPath);
+
+    // Set the position to overlay the QR code (right bottom)
+    $qrX = $imageWidth - imagesx($qrImage) - 50; // X-coordinate
+    $qrY = imagesy($certificateImage) - imagesy($qrImage) - 50; // Y-coordinate
+
+    // Copy the QR code image onto the certificate image
+    imagecopy($certificateImage, $qrImage, $qrX, $qrY, 0, 0, imagesx($qrImage), imagesy($qrImage));
+
+    // Output the resulting image
+    header('Content-Type: image/png');
+    header('Content-Disposition: attachment; filename="certificate.png"');
+    imagepng($certificateImage);
+
+    // Clean up resources
+    imagedestroy($certificateImage);
+    imagedestroy($qrImage);
+}
+
 	
 }
