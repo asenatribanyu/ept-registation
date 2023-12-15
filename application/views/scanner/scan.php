71bec379a59296d3f51d81543dd7719d31fdea70
MUHAMMAD GILANG <gilang.ariyana@widyatama.ac.id>
2023-12-15 17:25:10 +0700

    View scan.php dan Controller Scan.php (Contoroller nya masih kosong)

---
+++ application/views/scanner/scan.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>English Proficency Test | Lembaga Bahasa Widyatama</title>
+    <link rel="icon" sizes="76x76" href="https://ept-lembagabahasa.widyatama.ac.id/registration/assets/img/logo_widyatama.ico">
+    <link rel="icon" type="image/png" href="https://ept-lembagabahasa.widyatama.ac.id/registration/assets/img/logo_widyatama.ico">
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/custom/css/custom.css" rel="stylesheet">
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Nixie+One' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/fontawesome.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/templatemo-edu-meeting.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/owl.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/lightbox.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/styless.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.css">
+
+
+</head>
+
+<body>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">Nama </h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+            <h5 class="card-title">NPM</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+            <h5 class="card-title">Fakultas</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+            <h5 class="card-title">Prodi</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+            <h5 class="card-title">Score Test</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+            
+            
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <h2 class="mb-4">Detail Data Diri Mahasiswa</h2>
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Nama Mahasiswa</h5>
+                <p class="card-text" id="nama"><?php echo $nama; ?></p>
+                
+                <h5 class="card-title">NPM</h5>
+                <p class="card-text" id="npm"><?php echo $npm; ?></p>
+                
+                <h5 class="card-title">Fakultas</h5>
+                <p class="card-text" id="fakultas"><?php echo $fakultas; ?></p>
+                
+                <h5 class="card-title">Program Studi</h5>
+                <p class="card-text" id="prodi"><?php echo $prodi; ?></p>
+                
+                <h5 class="card-title">Jumlah Pengulangan Ujian</h5>
+                <p class="card-text" id="jumlah_pengulangan"><?php echo $jumlah_pengulangan; ?></p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Scripts -->
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/isotope.min.js"></script>
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/lightbox.js"></script>
+    <script src="assets/js/tabs.js"></script>
+    <script src="assets/js/video.js"></script>
+    <script src="assets/js/slick-slider.js"></script>
+    <script src="assets/js/custom.js"></script>
+    <script src="http://code.jquery.com/jquery-3.1.1.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/custom/js/booking.js" type="text/javascript"></script>
+
+</body>
+
+</html>
+
+
